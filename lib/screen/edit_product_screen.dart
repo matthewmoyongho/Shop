a3d233b92b31cabeb7034c59558cd44482f61646
matthewmoyongho <moyonghomatthew@gmail.com>
2022-02-17 16:06:08 +0100

    more logic added to edit product screen

--- lib/screen/edit_product_screen.dart
+++ lib/screen/edit_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
@@ -11,11 +12,29 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  void _uodateImageUrl() {
+    if (_imageFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
   @override
   void dispose() {
+    _imageFocusNode.removeListener(_uodateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageFocusNode.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    _imageFocusNode.addListener(_uodateImageUrl);
+    super.initState();
   }
 
   @override
@@ -24,35 +43,73 @@ class _EditProductScreenState extends State<EditProductScreen> {
       appBar: AppBar(
         title: const Text('Edit Product'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Form(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Title'),
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  },
-                ),
-                TextFormField(
-                  focusNode: _priceFocusNode,
-                  decoration: InputDecoration(labelText: 'Price'),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.number,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
-                  },
-                ),
-                TextFormField(
-                  focusNode: _descriptionFocusNode,
-                  decoration: InputDecoration(labelText: 'Decription'),
-                  maxLines: 3,
-                  keyboardType: TextInputType.multiline,
-                ),
-              ],
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context).requestFocus(_priceFocusNode);
+                    },
+                  ),
+                  TextFormField(
+                    focusNode: _priceFocusNode,
+                    decoration: InputDecoration(labelText: 'Price'),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.number,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context)
+                          .requestFocus(_descriptionFocusNode);
+                    },
+                  ),
+                  TextFormField(
+                    focusNode: _descriptionFocusNode,
+                    decoration: InputDecoration(labelText: 'Decription'),
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      //'assets/images/onboard1.jpg'
+                      //https://i.pinimg.com/originals/58/bd/4f/58bd4fc9ebfccc1f2de419529bbf1a12.jpg
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.only(top: 8, right: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        )),
+                        child: _imageUrlController.text.isEmpty
+                            ? Text('Enter image Ulr')
+                            : FittedBox(
+                                child: Image.network(
+                                  _imageUrlController.text,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          controller: _imageUrlController,
+                          focusNode: _imageFocusNode,
+                          decoration: InputDecoration(labelText: 'Images URL'),
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
